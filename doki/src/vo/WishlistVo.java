4336d8154739107225469022311ce909cee4260e
SHYKUMA <pp613469@gmail.com>
2025-03-12 19:22:20 +0900

    Update WishlistVo.java

--- doki/src/vo/WishlistVo.java
+++ doki/src/vo/WishlistVo.java
@@ -2,8 +2,8 @@ package vo;
 
 public class WishlistVo {
 	
-	 private int wishlist_id;   // 찜 고유 ID 
-	 private String package_id; // 패키지 고유 번호
+	 private int wishlist_id;   // 찜 고유 ID
+	 private String package_id;    // 패키지 고유 번호
 	 private int account_id;    // 계정 고유번호
 	 private String created_at;	// 찜한 날짜
 	 
@@ -16,8 +16,8 @@ public class WishlistVo {
 	public String getPackage_id() {
 		return package_id;
 	}
-	public void setPackage_id(String package_id) {
-		this.package_id = package_id;
+	public void setPackage_id(String string) {
+		this.package_id = string;
 	}
 	public int getAccount_id() {
 		return account_id;
